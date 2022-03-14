b97c5c9c543092ec17cd0f8cc303b4b66af05ce6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-14 18:53:22 +0100

    setup add and remove book functions

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-const bookTitle = document.querySelector('.title')
-const bookAuthor = document.querySelector('.author')
-const addButton = document.querySelector('.add')
-const removeButton = document.querySelectorAll('.remove')
-const bookList = document.querySelector('.booklist')
+const bookTitle = document.querySelector('.title');
+const bookAuthor = document.querySelector('.author');
+const addButton = document.querySelector('.add');
+const bookList = document.querySelector('.booklist');
 
-const bookCollection = []
+let bookCollection = [];
 
-addButton.addEventListener('click', () => {
-  bookCollection.push(
-    {
-      title: bookTitle.value,
-      author: bookAuthor.value
-    }
-  )
-  bookList.innerHTML +=
-    ` <p>${bookTitle.value}</p>
-      <p>${bookAuthor.value}</p>
-      <button type="button" class="remove">Remove</button>`
-})
+function CreateBook(title, author) {
+  this.id = title;
+  this.title = title;
+  this.author = author;
+}
 
-bookList.addEventListener('click', e => {
-  if (e.target.classList === 'remove') {
-    e.target.parentElement.remove()
-  }
-})
+function bookTemplate(title, author) {
+  bookList.innerHTML
+    += `
+    <div id="${title}" class="book">
+      <p>${title}</p>
+      <p>${author}</p>
+      <button type="button" id="${title}" class="remove" >Remove</button>
+    </div>
+    `;
+  const removeButton = document.querySelectorAll('.remove');
+  removeButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      bookCollection = bookCollection.filter((book) => button.id !== book.id);
+      bookList.innerHTML = '';
+      bookCollection.forEach((book) => {
+        bookTemplate(book.title, book.author);
+      });
+    });
+  });
+}
+
+function addandRemoveBook() {
+  addButton.addEventListener('click', () => {
+    bookCollection.push(new CreateBook(bookTitle.value, bookAuthor.value));
+    bookTemplate(bookTitle.value, bookAuthor.value);
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  });
+}
+addandRemoveBook();
